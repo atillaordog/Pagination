442260928c099253b98239cbb1e03a8fa517a2d8
Ördög Atilla <attila.ordog@yahoo.com>
2020-12-07 14:24:48 +0200

    Create autoload.php

---
+++ autoload.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Autoload file that needs to be laoded to use the ValidationWall
+ */
+
+if (!defined('PAGINATION_ROOT')) {
+    define('PAGINATION_ROOT', dirname(__FILE__) . DIRECTORY_SEPARATOR);
+}
+
+spl_autoload_register('autoload');
+
+function autoload($class)
+{
+	if ( class_exists($class,FALSE) ) {
+		//    Already loaded
+		return FALSE;
+	}
+	
+	$class = str_replace('Pagination\\', '', $class);
+	$class = str_replace('\\', '/', $class);
+	
+	$is_file = false;
+	if ( file_exists(PAGINATION_ROOT.$class.'.php') )
+	{
+		$is_file = PAGINATION_ROOT.$class.'.php';
+	}
+	
+	if ( $is_file !== false )
+	{
+		require($is_file);
+	}
+	
+	return false;
+}
